eb34fb668b3e87fcb629118537bfff1b792a7352
KATHIRVEL R <25d148@psgitech.ac.in>
2025-12-11 15:31:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4092189688-c74a7",
+  "appId": "1:651059136924:web:0070ef26644101f9fb2059",
+  "apiKey": "AIzaSyDnqpDkmttNbyPcZadBKMOrPjZLSN0SNyo",
+  "authDomain": "studio-4092189688-c74a7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "651059136924"
+};
