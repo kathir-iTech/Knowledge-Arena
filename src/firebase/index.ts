0056e8626970811b65dc9eabb65cf15ec9c950f7
KATHIRVEL R <25d148@psgitech.ac.in>
2026-01-22 16:46:43 +0000

    Try fixing this error: `Console Error: [2026-01-22T16:46:44.486Z] @fireb

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -5,23 +5,13 @@ import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore'
 
-// IMPORTANT: DO NOT MODIFY THIS FUNCTION
+// Initializes and returns the Firebase services.
 export function initializeFirebase() {
-  if (!getApps().length) {
-    let firebaseApp;
-    try {
-      firebaseApp = initializeApp();
-    } catch (e) {
-      if (process.env.NODE_ENV === "production") {
-        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
-      }
-      firebaseApp = initializeApp(firebaseConfig);
-    }
-
-    return getSdks(firebaseApp);
+  if (getApps().length) {
+    return getSdks(getApp());
   }
-
-  return getSdks(getApp());
+  const firebaseApp = initializeApp(firebaseConfig);
+  return getSdks(firebaseApp);
 }
 
 export function getSdks(firebaseApp: FirebaseApp) {
